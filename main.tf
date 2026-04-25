32b850b42e221d2e34284c8f73bb0f61dca6bcc2
Yuvaraj <yuvicardy18@gmail.com>
2026-04-25 16:00:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,9 +4,9 @@ provider "aws" {
 
 resource "aws_instance" "my-instance" {
   ami           = "ami-0e12ffc2dd465f6e4"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   subnet_id = "subnet-0f84c7e8c9f149767"
-  key_name = "yuvaraj-key"
+  key_name = "Yuvaraj-key"
   security_groups = ["sg-07908069173d6bea5"]
 tags = {
  Name = "Yuva_terraform"
